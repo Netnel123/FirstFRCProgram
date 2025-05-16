650108c4a37a22dad23540e7aef1fb63b2b8401a
ASUS <netanelekshtein@gmail.com>
2025-05-16 18:26:41 +0300

    crate the class : mine Dare: i crate constructor and crate the function isValidDate

---
+++ programe/Date.java
@@ -0,0 +1,67 @@
+package programe;
+
+public class Date {
+    private int day;
+    private int month;
+    private int year;
+    public Date(int day,int month,int year){
+      this.day=day;
+      this.month=month;
+      this.year=year;
+      if (isValidDate()==false){
+          year = 2000;
+          month = 1;
+          day = 1;
+          System.out.println("not legal");
+      }else {
+          System.out.println("legal");
+      }
+    }
+    private boolean isValidDate(){
+        switch (month){
+            case 1:
+                if (day>31){return false;}
+                break;
+            case 2:
+                if (day>28){return false;}
+                break;
+            case 3:
+                if (day>31){return false;}
+                break;
+            case 4:
+                if (day>30){return false;}
+                break;
+            case 5:
+                if (day>31){return false;}
+                break;
+            case 6:
+                if (day>30){return false;}
+                break;
+            case 7:
+                if (day>31){return false;}
+                break;
+            case 8:
+                if (day>31){return false;}
+                break;
+            case 9:
+                if (day>30){return false;}
+                break;
+            case 10:
+                if (day>31){return false;}
+                break;
+            case 11:
+                if (day>30){return false;}
+                break;
+            case 12:
+                if (day>31){return false;}
+                break;
+        }
+        if (month>12){
+            return false;
+        }
+        return true;
+    }
+    private boolean isLeapYear (int year) {
+        return (this.year%4==0 && this.year%100!=0) || (this.year%400==0) ? true : false;
+    }
+}
---
+++ programe/Mine.java
@@ -0,0 +1,7 @@
+package programe;
+
+public class Mine {
+    public static void main(String[] args) {
+        Date x = new Date(30,6,200);
+    }
+}

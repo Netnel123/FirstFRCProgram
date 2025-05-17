47db0c6919dc8b463811c508205866488205be7b
ASUS <netanelekshtein@gmail.com>
2025-05-17 23:15:02 +0300

    add Weight

--- programe/Mine.java
+++ programe/Mine.java
@@ -2,6 +2,16 @@ package programe;
 
 public class Mine {
     public static void main(String[] args) {
-        Date x = new Date(30,6,200);
+        Date x = new Date(30,6,2000);
+        System.out.println(x.toString());
+        Date b = new Date(30,6,2000);
+        System.out.println(x.equals(b));
+        System.out.println(x.before(b));
+        x.addDays(14);
+        System.out.println(x.toString());
+        Weight w = new Weight(2,10045);
+        System.out.println(w.toString());
+        w = w.add(new Weight(10,20000));
+        System.out.println(w.toString());
     }
 }

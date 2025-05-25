c0f20603f7f7805d990da8e8dbd6c4585f98ae1f
ASUS <netanelekshtein@gmail.com>
2025-05-25 21:29:06 +0300

    fiks ClonBaby EQUALS updateCurrentWeight : I'm not finished with the function yet (I'm not sure the calculations are correct)

--- programe/Baby.java
+++ programe/Baby.java
@@ -16,13 +16,11 @@ public class Baby {
         this.birthWeight = birthWeight;
         currentWeight = this.birthWeight;
     }
-    public Baby(Baby other){
-        other.birthWeight=birthWeight;
-        other.birthDate = birthDate;
-        other.name = name;
-        other.id = id;
-        other.currentWeight = currentWeight;
+    public Baby ClonBaby(Baby other){
+        other = this;
+        return other;
     }
+
 
     public String getName(){
         return name;
@@ -57,35 +55,49 @@ public class Baby {
         return stut;
     }
     public boolean equals (Baby other){
-        if (other == this){
+        if (other.currentWeight == this.currentWeight&&
+                other.id == this.id&&
+                other.name == this.name&&
+                other.birthDate == this.birthDate&&
+                other.birthWeight == this.birthWeight){
             return true;
         }
         else {
             return false;
         }
     }
+
     public void updateCurrentWeight (int grams){
         if(grams+currentWeight.grams<0){
 
         }else{
-          currentWeight= currentWeight.add(new Weight(0,grams));
+            currentWeight.add(new Weight(0,grams));
         }
     }
     public boolean isWeightInValidRange(Date date){
         if (birthDate.month == date.month){
             if (birthDate.day>=date.day-6){
-               if (birthWeight.grams-10>currentWeight.grams){
-                   return false;
-               }else {
+               if (currentWeight.lessThan(birthWeight.add(new Weight(0,-10)))){
                    return true;
+               }else {
+                   return false;
                }
-            } else if (birthDate.day>=date.day-30){
-                if(currentWeight.grams>birthWeight.grams+((date.day-6)*30)){
-                    return true;
-                }else {
+            } else {
+                if (currentWeight.lessThan(birthWeight.add(
+                        new Weight(0,30*(date.day-birthDate.day))))){
                     return false;
+                }else {
+                    return true;
                 }
             }
+        }else if(birthDate.month+1==date.month){
+            if (currentWeight.lessThan(birthWeight.add(
+                    new Weight(0,30*(24+(date.day-birthDate.day)))))){
+                return false;
+            }else {
+                return true;
+            }
+            //אד לפו אסיתי לבנטיים(אני לא בטוח שדרך החישוב שעסיתי נחון)
         }else if(birthDate.month+4 >= date.month){
             if (currentWeight.grams>=birthWeight.grams+(date.month*750)){
                 return true;
